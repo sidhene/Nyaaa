7b23cffdbea1bb2e555117a3eab390bd10514c56
alfredlam42 <alfred.lam.42@gmail.com>
2015-11-07 18:56:17 -0800

    add feature to send messages, fix friend list, fix message list

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,11 +5,15 @@ class WelcomeController < ApplicationController
   end
 
   def meow
-    @messages = Message.where(receiver_id: 1)
-    friend_list_id = FriendList.find_by(user_id: 1).list
+    @messages = Message.where(receiver_id: current_user.id)
     @friend_list = []
-    friend_list_id.each do |id|
+    if FriendList.find_by(user_id: current_user.id)
+      friend_list_id = FriendList.find_by(user_id: current_user.id).list
+      friend_list_id.each do |id|
       @friend_list << User.find(id.to_i)
+    end
+
+
     end
   end
 
